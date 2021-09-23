4333ee86077f28b06d359ae8fc83aa9f3b04900d
akahrsh21 <85753829+akahrsh21@users.noreply.github.com>
2021-09-23 12:03:37 +0530

    Update Controller.java

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @GetMapping("/get")
+    @GetMapping("/")
     public String testGetMapping(){
         return "Hello Akahrsh";
     }
